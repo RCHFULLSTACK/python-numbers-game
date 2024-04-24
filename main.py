66a58142b588b9e78c2c677415241b1e972c39c7
RCH FULLSTACK <132129040+RCHFULLSTACK@users.noreply.github.com>
2024-04-24 13:03:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,24 +4,23 @@ from game import Game
 
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Set up screen and constant definitions
 screen_width = 600
 screen_height = 500
-# Initialize Pygame
+
 pygame.init()
 pygame.mixer.init()
-# Set up the game window
+
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("World Of Guessing Numbers! V.1")
-# Set up game icon
+
 icon = pygame.image.load("assets/image/icon_image.png")
 pygame.display.set_icon(icon)
-# Load background music and play continuously
+
 pygame.mixer.music.load("assets/sound/background_music.mp3")
 pygame.mixer.music.play(-1)
-# Load background image
+
 background_image = pygame.image.load("assets/image/background_image.jpg").convert()
-# Load number images
+
 number_images = [pygame.image.load(os.path.join('assets', 'image', f"number_{i}.png")).convert_alpha()
                  for i in range(1, 11)]
 
